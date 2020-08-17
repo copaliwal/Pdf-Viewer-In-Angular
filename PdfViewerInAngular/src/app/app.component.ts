fcb4f2d91c8920090b3b2bafb2bad101357239f1
Chandrashekhar Paliwal <C.Paliwal@wolterskluwer.com>
2020-08-17 13:03:48 +0530

    Api call & Displayed PDF on page in iFrame, object and embed

--- PdfViewerInAngular/src/app/app.component.ts
+++ PdfViewerInAngular/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
- 
+export class AppComponent implements OnInit {
+  dataLocalUrl: any
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.showPdfFile();
+  }
+
+  // This method is calling API endpoint to get PDF file
+  getPDF(): Observable<any> {
+    var url: string = 'http://localhost:7071/api/pdffile';
+    return this.http.get(url, { responseType: 'blob' as 'json' });
+  }
+
+  // this method is reading rsponce received in above mentod from API
+  private showPdfFile() {
+    this.getPDF().subscribe(
+      (response) => {
+        var pdfFile = new Blob([response], { type: 'application/pdf' });
+        var path = URL.createObjectURL(pdfFile);
+        this.dataLocalUrl = this.sanitizer.bypassSecurityTrustResourceUrl(path);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
 }
